c1c8e0d75ad27e8daf45881d5bf101231b1b092d
David DACRUZ <dadacruz@e2r2p12.42.fr>
2019-08-11 19:38:24 +0200

    possible matches component

--- app/src/components/PossibleMatches.vue
+++ app/src/components/PossibleMatches.vue
@@ -2,9 +2,13 @@
   <div
     class="container bg-white mx-auto w-full max-w-xl px-3 py-4 shadow rounded border-t-4 border-teal-500 mt-12"
   >
-    <ul>
-        <li v-for="matches in possible_matches" :key="matches.id">{{matches.username}}</li>
-    </ul>
+        <div v-for="matches in possible_matches" :key="matches.id" class="w-full flex justify-between items-center">
+            <img :src="'/api/v1/images/get/' + matches.avatar" alt="" class="rounded-full w-32 h-32">
+            {{matches.username}}
+            <button class="bg-teal-600 text-white p-1 rounded">
+                Like
+            </button>
+        </div>
   </div>
 </template>
 
