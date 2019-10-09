c8e4d0ff3ff9279e100a6125a189eda19b1e8982
David Dacruz <dada@42.fr>
2019-10-09 12:05:16 +0200

    je suis trop chaud

--- app/src/components/ui/Tabs.vue
+++ app/src/components/ui/Tabs.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <div class="w-full bg-teal-600 text-white py-2 text-xs uppercase">
+    <div class="hidden md:block w-full bg-teal-600 text-white py-2 text-xs uppercase">
       <ul class="list-reset flex items-center text-xs">
         <li
           v-for="tab in tabs"
-          :class="{ 'bg-white text-teal-600 rounded-full px-1 shadow-lg': tab.isActive }"
+          :class="{ 'bg-white text-teal-600 rounded-full px-1 shadow-lg ': tab.isActive }"
           class="ml-2"
+          :key="tab.name"
+        >
+          <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="block md:hidden bg-teal-600 text-white py-2 text-xs uppercase flex-col">
+      <div v-if="!show"@click="show = !show" class="mx-6 text-white cursor-pointer">+</div>
+      <div v-else @click="show = !show" class="mx-6 text-white cursor-pointer">-</div>
+    <ul v-if="show" class="list-reset flex-col items-center text-xs">
+        <li
+          v-for="tab in tabs"
+          :class="{ '': tab.isActive }"
+          class="ml-2 py-2"
           :key="tab.name"
         >
           <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
@@ -23,7 +37,8 @@
 export default {
   data() {
     return {
-      tabs: []
+      tabs: [],
+      show: false
     };
   },
   created() {
